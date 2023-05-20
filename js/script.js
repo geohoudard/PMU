bda0aa710638bf7f18178fdfa27df3bc4f20f9c6
Comité Champagne <122040369+geohoudard@users.noreply.github.com>
2023-05-20 23:01:04 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
 <script>
+// API YouTube
+const apiKey = 'AIzaSyAR4bvoqYM97PFWr_w8ChXV4YeFq7DvX0c';
+
+// ID de la playlist que vous souhaitez afficher.
+const playlistId = 'CIVC';
+
+// Récupération de la liste des vidéos de la playlist.
+fetch(`https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=${playlistId}&key=${AIzaSyAR4bvoqYM97PFWr_w8ChXV4YeFq7DvX0c}`)
+  .then(response => response.json())
+  .then(data => {
+    // Pour chaque vidéo de la playlist, créez un lecteur vidéo YouTube et ajoutez-le à la page.
+    data.items.forEach(item => {
+      const videoId = item.snippet.resourceId.videoId;
+      const playerDiv = document.getElementById('player');
+      const iframe = document.createElement('iframe');
+      iframe.src = `https://www.youtube.com/embed/${videoId}`;
+      iframe.width = '560';
+      iframe.height = '315';
+      iframe.allowFullscreen = true;
+      playerDiv.appendChild(iframe);
+    });
+  });
+
   // Récupération du formulaire
   const form = document.querySelector('form');
 
